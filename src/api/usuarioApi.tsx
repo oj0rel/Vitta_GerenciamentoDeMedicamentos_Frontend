62c32fd03aabeadcfb09d8bb3c126581c50084fe
oj0rel <ogabrieusiuva@gmail.com>
2025-10-28 11:57:42 -0300

    Construção da chamada do método de cadastrar usuário, do backend.

--- src/api/usuarioApi.tsx
+++ src/api/usuarioApi.tsx
@@ -1,4 +1,4 @@
-import { LoginResponse, UsuarioLoginData } from "@/src/types/authTypes";
+import { CadastroResponse, LoginResponse, UsuarioCadastroData, UsuarioLoginData } from "@/src/types/authTypes";
 import apiManager from "./apiManager";
 
 export const usuarioLogin = async (data: UsuarioLoginData): Promise<LoginResponse> => {
@@ -10,3 +10,13 @@ export const usuarioLogin = async (data: UsuarioLoginData): Promise<LoginRespons
     throw error;
   }
 };
+
+export const usuarioCadastro = async (data: UsuarioCadastroData): Promise<CadastroResponse> => {
+  try {
+    const response = await apiManager.post<CadastroResponse>('api/usuarios/cadastrar', data);
+    return response.data;
+  } catch (error) {
+    console.error("Erro na chamada de cadastro: ", error);
+    throw error;
+  }
+}
