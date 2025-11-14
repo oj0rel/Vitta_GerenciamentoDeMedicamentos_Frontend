3a974b96be4fe995723607f4c25c66e9193ea8d8
oj0rel <ogabrieusiuva@gmail.com>
2025-11-14 19:15:47 -0300

    Endereço da API online, hospedada no servidor.

--- src/api/apiManager.ts
+++ src/api/apiManager.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-// 192.168.100.10 ip da máquina
-// 10.136.36.30
 export const apiManager = axios.create({
-  baseURL: 'http://192.168.100.10:8080',
+  baseURL: 'http://academico3.rj.senac.br/vitta',
   responseType: 'json',
   headers: {
     'Content-Type': 'application/json',
